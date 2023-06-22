fb074a5f86a391293e9f4f460b7ca17663efb454
gpanaretou <gpanaretou@hotmail.com>
2023-06-22 13:20:59 +0300

    added create user from front end functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,16 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:notice] = "User was created successfully."
+            flash[:notice] = "Welcome to Alpha Blog, you have successfully signed up!"
             redirect_to '/'
         else
             render :new, status: :unprocessable_entity
-            
         end
     end
 
+    privategit
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 
 end
