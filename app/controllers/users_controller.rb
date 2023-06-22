165225b1c5c9cf9f965b42dd5319e47af675587c
gpanaretou <gpanaretou@hotmail.com>
2023-06-22 13:27:44 +0300

    fixed homepage error due to signup button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
         end
     end
 
-    privategit
+    private
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
