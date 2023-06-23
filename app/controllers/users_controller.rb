8aff7c7b5d7de6b1f32c338713befd3e1a0fd3c3
gpanaretou <gpanaretou@hotmail.com>
2023-06-23 22:07:53 +0300

    added server side restrictions to actions for articles and users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def new
         @user = User.new
@@ -49,6 +50,13 @@ class UsersController < ApplicationController
 
     def set_user
         @user = User.find(params[:id])
+    end
+
+    def require_same_user
+        if !(logged_in? && current_user == @user)
+            flash[:alert] = "You can only edit your profile"
+            redirect_to @user
+        end
     end
 
 
