0fec43092b1361d54cdc588386b2ff9631575852
gpanaretou <gpanaretou@hotmail.com>
2023-06-23 16:10:37 +0300

    added login and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,17 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "pages#home"
-  get "about", to: "pages#about"
-  get "signup", to: "users#new"
-  
-  get "login", to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
 
   resources :articles
   resources :users, except: [:new]
+
+  get "about", to: "pages#about"
+  get "signup", to: "users#new"
+  
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+
+
 
 end
