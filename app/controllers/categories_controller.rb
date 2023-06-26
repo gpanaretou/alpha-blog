35c739ce5b801a90e8b0f24733227f034f0c8a83
gpanaretou <gpanaretou@hotmail.com>
2023-06-26 18:02:59 +0300

    added create category form and added tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
 
     def new
+        @category = Category.new
 
     end
 
@@ -11,5 +12,24 @@ class CategoriesController < ApplicationController
     def index
 
     end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "Category was successfully created"
+            redirect_to @category
+        else
+            render 'new', status: :unprocessable_entity 
+        end
+    end
+
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
+    end
+
+
 
 end
