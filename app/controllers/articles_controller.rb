64871c0f25abaf692087b7e436493ae3de06b9e2
gpanaretou <gpanaretou@hotmail.com>
2023-06-29 10:31:42 +0300

    edited new article form to include categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,7 @@ class ArticlesController < ApplicationController
 
     def new
         @article = Article.new
+        
     end
 
     def create
@@ -52,7 +53,7 @@ class ArticlesController < ApplicationController
     end
 
     def article_params
-        params.require(:article).permit(:title, :description)
+        params.require(:article).permit(:title, :description, category_ids: [] )
     end
 
     def require_same_user
